038ebc9f4d28f215528e0a31092bd383b997823c
KAWACHI Takashi <kawachi@p-lucky.net>
2013-02-03 22:45:14 +0900

    Notify meeting

--- clock.rb
+++ clock.rb
@@ -1,9 +1,37 @@
+# coding: utf-8
 require 'rubygems'
 require 'clockwork'
-include Clockwork
+require 'hipchat-api'
 
-handler do |job|
-  # do something
+if !ENV['HIPCHAT_API_KEY'] || !ENV['ROOM']
+  puts 'HIPCHAT_API_KEY or ROOM env is not set'
+  exit(1)
 end
 
-every(1.day, 'job_name')
+
+class Time
+  def home_day?
+    tuesday? || wednesday? || thursday? || friday?
+  end
+
+  def office_day?
+    monday?
+  end
+
+  def weekday?
+    home_day? || office_day?
+  end
+end
+
+Clockwork.configure do |config|
+  config[:tz] = 'Asia/Tokyo'
+end
+
+module Clockwork
+  every(1.day, 'meeting.notification', at: %w(9:50 14:50), if: lambda { |t| t.weekday? }) do
+    message = 'あと10分でミーティングでーす！'
+    hipchat_api = HipChat::API.new(ENV['HIPCHAT_API_KEY'])
+    from = ENV['FROM'] || 'bot'
+    hipchat_api.rooms_message(ENV['ROOM'], from, message, 1)
+  end
+end
